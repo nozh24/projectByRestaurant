63464b31bfef2991308f65cd4a53022815c0fed9
Nasty <ozhgibesova.an@pit.local>
2021-09-27 14:11:02 +0500

    changes files

--- assets/script/calendar.js
+++ assets/script/calendar.js
@@ -1,40 +1,40 @@
-function Calendar(id, year, month) {
-var Dlast = new Date(year,month+1,0).getDate(),
-    Data = new Date(year,month,Dlast),
-    DNlast = new Date(Data.getFullYear(),Data.getMonth(),Dlast).getDay(),
-    DNfirst = new Date(Data.getFullYear(),Data.getMonth(),1).getDay(),
-    calendar = '<tr>',
-    month=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
-if (DNfirst != 0) {
-  for(var  i = 1; i < DNfirst; i++) calendar += '<td>';
-}else{
-  for(var  i = 0; i < 6; i++) calendar += '<td>';
-}
-for(var  i = 1; i <= Dlast; i++) {
-  if (i == new Date().getDate() && Data.getFullYear() == new Date().getFullYear() && Data.getMonth() == new Date().getMonth()) {
-    calendar += '<td class="today">' + i;
-  }else{
-    calendar += '<td>' + i;
-  }
-  if (new Date(Data.getFullYear(),Data.getMonth(),i).getDay() == 0) {
-    calendar += '<tr>';
-  }
-}
-for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
-document.querySelector('#'+id+' tbody').innerHTML = calendar;
-document.querySelector('#'+id+' thead td:nth-child(2)').innerHTML = month[Data.getMonth()] +' '+ Data.getFullYear();
-document.querySelector('#'+id+' thead td:nth-child(2)').dataset.month = Data.getMonth();
-document.querySelector('#'+id+' thead td:nth-child(2)').dataset.year = Data.getFullYear();
-if (document.querySelectorAll('#'+id+' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
-    document.querySelector('#'+id+' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
-}
-}
-Calendar("calendar2", new Date().getFullYear(), new Date().getMonth());
-// переключатель минус месяц
-document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
-  Calendar("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month)-1);
-}
-// переключатель плюс месяц
-document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
-  Calendar("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month)+1);
-}
+// function Calendar(id, year, month) {
+// var Dlast = new Date(year,month+1,0).getDate(),
+//     Data = new Date(year,month,Dlast),
+//     DNlast = new Date(Data.getFullYear(),Data.getMonth(),Dlast).getDay(),
+//     DNfirst = new Date(Data.getFullYear(),Data.getMonth(),1).getDay(),
+//     calendar = '<tr>',
+//     month=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
+// if (DNfirst != 0) {
+//   for(var  i = 1; i < DNfirst; i++) calendar += '<td>';
+// }else{
+//   for(var  i = 0; i < 6; i++) calendar += '<td>';
+// }
+// for(var  i = 1; i <= Dlast; i++) {
+//   if (i == new Date().getDate() && Data.getFullYear() == new Date().getFullYear() && Data.getMonth() == new Date().getMonth()) {
+//     calendar += '<td class="today">' + i;
+//   }else{
+//     calendar += '<td>' + i;
+//   }
+//   if (new Date(Data.getFullYear(),Data.getMonth(),i).getDay() == 0) {
+//     calendar += '<tr>';
+//   }
+// }
+// for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
+// document.querySelector('#'+id+' tbody').innerHTML = calendar;
+// document.querySelector('#'+id+' thead td:nth-child(2)').innerHTML = month[Data.getMonth()] +' '+ Data.getFullYear();
+// document.querySelector('#'+id+' thead td:nth-child(2)').dataset.month = Data.getMonth();
+// document.querySelector('#'+id+' thead td:nth-child(2)').dataset.year = Data.getFullYear();
+// if (document.querySelectorAll('#'+id+' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
+//     document.querySelector('#'+id+' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
+// }
+// }
+// Calendar("calendar2", new Date().getFullYear(), new Date().getMonth());
+// // переключатель минус месяц
+// document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
+//   Calendar("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month)-1);
+// }
+// // переключатель плюс месяц
+// document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
+//   Calendar("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month)+1);
+// }
---
+++ assets/script/validation.js
@@ -0,0 +1,40 @@
+const fullName = document.getElementById("full_name"),
+      email = document.getElementById("email"),
+      phone = document.getElementById("phone"),
+      form = document.getElementById("data_form"),
+      errorElement = document.getElementById("error");
+
+form.addEventListener('submit', (e) =>{
+    let regexPhone = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+    let regexEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    let regexName =  /^[А-Яа-яЁё]+(\s+[А-Яа-яЁё]+)+$/;
+    let messages = [];
+    //ФИО
+    if (fullName.value === "" || fullName.value === null || fullName.value.length < 5 ){
+        messages.push("Введите полное ФИО");
+    }
+    else if(!regexName.test(fullName.value)){
+        messages.push('Введите коректное ФИО');
+    }
+
+    //номер телефона
+    if (phone.value === "" || phone.value === null ){
+        messages.push("Введите ФИО");
+    }
+    else if (!regexPhone.test(phone.value)){
+        messages.push('Неверный номер телефон');
+    }
+
+    //эл.почта
+    if (email.value === "" || email.value === null ){
+        messages.push("Введите электронную почту");
+    }
+    else if (!regexEmail.test(email.value)){
+        messages.push('Неверная электронная почта');
+    }
+    //работа с message
+    if (messages.length > 0){
+        e.preventDefault();
+        errorElement.innerText = messages.join(", ");
+    } 
+})
