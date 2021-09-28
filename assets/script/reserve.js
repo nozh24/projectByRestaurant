96e2eec0283fdb1c0f8b6287e08c924c61b61740
nozh24 <n.ozh24@mail.ru>
2021-09-28 11:47:49 +0500

    forms

--- assets/script/reserve.js
+++ assets/script/reserve.js
@@ -3,9 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {})
 // преобразование в массив (Array.from(...))
 	const tables = Array.from(document.getElementsByClassName('allTables'));
 	const elemBtns = Array.from(document.getElementsByClassName('date_time_button'));
-	const date = document.getElementById('dateToday');
+	const date = document.querySelector('input');
 	const link = document.getElementById("buttonSend");
 	const btn = document.getElementsByClassName("button");
+
+	
+
 	
 	if (elemBtns != '' || elemBtns != null){
 		//перебор элементов массива
@@ -25,16 +28,36 @@ document.addEventListener("DOMContentLoaded", () => {})
 			});
 		};
 	}
-	// if (date.value != '' && elemBtn.classList ==  "activeTime" && elemTables.classList ==  "activeTable"){
-	// 	link.classList.remove("no_link");
-	// 	btn.classList.remove("btnDisabled");
-	// }
-	// else{
-	// 	console.log(false);
-	// }
-	
+
+
 	function activeElements(activeElem, activeClass, event){
 		activeElem.forEach( elem => elem.classList.remove(activeClass));
 		event.target.classList.toggle(activeClass);
 	}
+
+	// разблокировка кнопки при выполнении всех условий
+	if (date.length > 0 && elemTables.className == "activeTime" && elemBtn.className == "activeTable"){
+		link.classList.remove("no_link");
+		btn.classList.remove("btnDisabled");
+	}
+	else{
+		console.log(false);
+	}
+	const itemsForm = document.getElementById('data_form');
+	const users = [];
+
+	function reserveUser(e){
+		const fullName = document.getElementById('full_name').value;
+		const email = document.getElementById('email').value;
+		const phone = document.getElementById('phone').value;
+		const comments = document.getElementById('comments').value;
+
+		e.preventDefault();
+		const user = { fullName, email, phone, comments};
+		users.push(user);
+		this.reset();
+
+	}
+	itemsForm.addEventListener('submit', reserveUser)
+
 
